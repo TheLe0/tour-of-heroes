f042939d72f66c6ef7c92f659a3cc61053c15045
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2022-10-15 12:34:27 -0300

    Chapter 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Hero } from '../models/hero';
 
@@ -7,18 +7,27 @@ import { Hero } from '../models/hero';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
 
-  public hero: Hero;
+  public heroes: Hero[];
+  selectedHero?: Hero;
 
   constructor() {
-    this.hero = {
-      id: 1,
-      name: "Windstorm"
-    }
+    this.heroes = [
+      { id: 12, name: 'Dr. Nice' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr. IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
   }
 
-  ngOnInit(): void {
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }

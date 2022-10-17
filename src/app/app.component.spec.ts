323e23fc0b923be45f8565c84320bd066e298a58
TheLe0 <leob.tosin@hotmail.com>
2022-10-17 20:27:42 -0300

    Fix app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +10,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        MessagesComponent
       ],
     }).compileComponents();
   });
@@ -23,13 +25,13 @@ describe('AppComponent', () => {
   it(`should have as title 'tour-of-heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tour-of-heroes');
+    expect(app.title).toEqual('Tour of Heroes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tour-of-heroes app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Tour of Heroes');
   });
 });
